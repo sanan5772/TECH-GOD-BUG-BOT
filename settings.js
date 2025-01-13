55ed8fd76cb94509db9d0d58c0f1a49cbb978e74
sanan5772 <sanan.khan.478.478@gmail.com>
2025-01-13 23:40:57 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,8 +10,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "9923119335047"
+global.ownername = "🐛ARHAM-XD"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
 global.location = "India, Up, Rampur"
